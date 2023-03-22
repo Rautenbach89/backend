cd3cf274fb3bed20aaa46129bd1d15585d1516a1
Rautenbach89 <105278662+Rautenbach89@users.noreply.github.com>
2023-03-22 07:52:20 +0100

    Update courses.js

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const coursesController = require("../../controllers/coursesController");
 const ROLES_LIST = require("../../config/roles_list");
-//const verifyRoles = require('../../middleware/verifyRoles');
 
 router
   .route("/")
