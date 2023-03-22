0748277fef14d2ef3247fc698db10fd9587b66b1
Rautenbach89 <105278662+Rautenbach89@users.noreply.github.com>
2023-03-22 07:50:24 +0100

    Update verifyJWT.js

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -5,7 +5,7 @@ const verifyJWT = (req, res, next) => {
   if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403); //invalid token
+    if (err) return res.sendStatus(403);
     req.user = decoded.UserInfo.username;
     req.roles = decoded.UserInfo.roles;
     next();
