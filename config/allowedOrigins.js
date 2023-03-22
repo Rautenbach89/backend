379589330674f1a73ea63fd2c349c376f9c8e25d
Rautenbach89 <105278662+Rautenbach89@users.noreply.github.com>
2023-03-22 07:45:57 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,8 @@
 const allowedOrigins = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
-  "http://192.168.178.58:3000",
-  "http://127.0.0.1:3000",
-  "http://localhost:3500",
-  "http://localhost:3000",
+  "http://lwivs26.gm.fh-koeln.de:3000",
+  "https://lwivs26.gm.fh-koeln.de:3000",
+  "http://139.6.56.155:3000",
+  "https://139.6.56.155:3000",
 ];
 
 module.exports = allowedOrigins;
